116a02b4f33466644e5d20cb5f5cb0c4e52e8f14
GiangVB <giangvb192@gmaiil.com>
2022-05-10 11:04:44 +0700

    service

---
+++ README.md
@@ -0,0 +1,1 @@
+# Thực hành AJAX ứng dụng quản lý điện thoại #
---
+++ src/main/java/com/codegym/service/Smartphone/ISmartphoneService.java
@@ -0,0 +1,7 @@
+package com.codegym.service.Smartphone;
+
+import com.codegym.model.Smartphone;
+import com.codegym.service.IGeneralService;
+
+public interface ISmartphoneService extends IGeneralService<Smartphone> {
+}
